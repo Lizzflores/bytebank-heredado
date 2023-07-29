fd85f2424cf333d646fa46e3e4fe856215cabc9d
ElizabethFlores <lizflores.dev@gmail.com>
2023-07-29 16:45:05 -0600

    Add Cliente and Test Cuenta class

---
+++ src/Cliente.java
@@ -0,0 +1,28 @@
+
+class Cliente{
+
+   private String nombre;
+   private String documento;
+   private String telefono;
+
+   public void setNombre(String nombre){
+    this.nombre = nombre;
+   }
+   public String getNombre(){
+    return nombre;
+   }
+
+   public void setDocumento(String documento){
+    this.documento = documento;
+   }
+   public String getDocumento(){
+    return documento;
+   }
+
+   public void setTelefono(String telefono){
+    this.telefono = telefono;
+   }
+   public String getTelefono(){
+    return telefono;
+   }
+}

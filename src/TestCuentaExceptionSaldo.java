aae94c689e95123cc94a304022570277844e8537
ElizabethFlores <lizflores.dev@gmail.com>
2023-08-14 21:01:03 -0600

    Refactor main method

--- src/TestCuentaExceptionSaldo.java
+++ src/TestCuentaExceptionSaldo.java
@@ -4,6 +4,11 @@ public class TestCuentaExceptionSaldo {
 	public static void main(String[] args) {
 		Cuenta cuenta =new CuentaAhorros(123, 456);
 		cuenta.depositar(200);
-		cuenta.retirar(210);
+		try {
+			cuenta.retirar(210);
+		} catch (SaldoInsuficienteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
